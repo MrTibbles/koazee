15c71a29d6f91b42f3a160b8aa711236267487be
Freddie Tibbles <freddie.tibbles@brandwidth.co.uk>
2015-02-18 14:18:20 +0000

    form submission

--- app/db_connect.php
+++ app/db_connect.php
@@ -1,4 +1,3 @@
-
 <?php
 $servername = "mysql-55.int.mythic-beasts.com";
 $username = "vaughnmck";
@@ -7,21 +6,28 @@ $dbname = "vaughnmck";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
-var_dump($conn);
-die();
 
 // Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-$sql = "INSERT INTO enquiries (email, group, destination, dates)
-VALUES ('$_POST[email]', '$_POST[group_size]', '$_POST[destination]', '$_POST[dates]')";
+$email = $_POST[email];
+$group_size = $_POST[group_size];
+$destination = $_POST[destination];
+$dates = $_POST[dates];
+
+// /VALUES ($_POST[email], $_POST[group_size], $_POST[destination], $_POST[dates])";
+$sql = "INSERT INTO enquiries (user_email, group_size, hol_destination, desired_dates)
+VALUES ('$email', '$group_size', '$destination', '$dates')";
+
 
 if (mysqli_query($conn, $sql)) {
     echo "New record created successfully";
+    header("Location: http://www.koazee.co");
 } else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    header("Location: http://www.koazee.co");
 }
 
 mysqli_close($conn);
